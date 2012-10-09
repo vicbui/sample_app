015850e92cc496b041943f38857b4ad44fa675c4
Vic Bui <quangvu88@hotmail.com>
2012-10-09 01:23:33 -0700

    Finish sign in

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
  FactoryGirl.define do
  	factory :user do
- 		email "quangvu88@example.com"
+ 		email "a@a.com"
  		name "VicBui"
  		password "Viclihai1521"
  		password_confirmation "Viclihai1521"
