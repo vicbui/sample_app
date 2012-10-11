cf9f327eac0c203117d1635e10cb7a261bf17b25
Vic Bui <quangvu88@hotmail.com>
2012-10-11 06:33:15 -0700

    Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,4 +6,13 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
 
   default_scope order: 'created_at DESC'
+
+   def self.from_user_followed_by(user)
+	 followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
+  end
+
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,14 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
-  
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key:"followed_id", 
+                                  class_name: "Relationship",
+                                  dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower                                
+
+
   before_save {|user| user.email=email.downcase}
   before_save :create_remember_token
 
@@ -25,8 +32,22 @@ class User < ActiveRecord::Base
 
 
   def feed
-    Micropost.where("user_id = ?",id)
+    Micropost.from_user_followed_by(self)
+    #Micropost.where("user_id = ?",id)
   end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
+  end
+
   private
 
     def create_remember_token
